c1883c5ab647b0327f9835fb2771496b4e8b92dc
Hugo Flor <hugo.flor@edu.uah.es>
2026-02-25 23:17:59 +0100

    Animacion sesion 2

--- lab02-animacion/lab02-animacion.py
+++ lab02-animacion/lab02-animacion.py
@@ -5,14 +5,31 @@ SCREEN_HEIGHT = 600
 
 class MiJuego(arcade.Window):
     def __init__(self):
-        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Mi Juego")
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "ANIMACIÓN BÁSICA")
         arcade.set_background_color(arcade.color.AIR_SUPERIORITY_BLUE)
-    
+        
+        # Posición central X de nuestro cuadrado
+        self.posicion_x = 100
+
     def on_draw(self):
         self.clear()
-        # Poner aquí el código del dibujo
-        # Cambiar la posición y/o tamaño del dibujo para crear animación
         
+        # Usamos tu función preferida:
+        # Left (Izquierda), Right (Derecha), Bottom (Abajo), Top (Arriba)
+        arcade.draw_lrbt_rectangle_filled(
+            left=self.posicion_x - 25,
+            right=self.posicion_x + 25,
+            bottom=275,  # Centro Y (300) menos 25
+            top=325,     # Centro Y (300) más 25
+            color=arcade.color.CANDY_APPLE_RED
+        )
+
+    def on_update(self, delta_time):
+        self.posicion_x += 3
+        
+        # Reiniciar si sale de la pantalla
+        if self.posicion_x > SCREEN_WIDTH + 25:
+            self.posicion_x = -25
 
 if __name__ == "__main__":
     juego = MiJuego()
