a05728653996b533c381686b91724c8ab2558250
Hugo <hugo.flor@edu.uah.es>
2026-03-26 20:11:41 +0100

    TERMINADA LA PRÁCTICA 9

--- lab09-walls/lab09-walls.py
+++ lab09-walls/lab09-walls.py
@@ -1,58 +1,58 @@
-""" Práctica Sprites and Walls """
-
 import arcade
 
 # --- Constants ---
 SPRITE_SCALING_BOX = 0.5
 SPRITE_SCALING_PLAYER = 0.5
 SPRITE_SCALING_WALL = 0.3
+SPRITE_SCALING_COIN = 0.3  # **NUEVO** Escala para la moneda
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
 MOVEMENT_SPEED = 5
 
-
 class MyGame(arcade.Window):
     """ This class represents the main window of the game. """
 
     def __init__(self):
         """ Initializer """
-        # Call the parent class initializer
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Sprites With Walls Example")
 
-        #Lista de Sprites
+        # Lista de Sprites
         self.player_list = None
         self.wall_list = None
+        self.coin_list = None  # **NUEVO** Lista para las monedas
 
-        #Configuramos el jugador
+        # Configuramos el jugador
         self.player_sprite = None
 
-        #Variable que contiene nuestro sencillo "motor de física"
+        # Variable que contiene nuestro sencillo "motor de física"
         self.physics_engine = None
 
     def setup(self):
-        #Elegimos el color de fondo
+        # Elegimos el color de fondo
         arcade.set_background_color(arcade.color.AMAZON)
 
-        #Listas de Sprites
+        # Listas de Sprites
         self.player_list = arcade.SpriteList()
         self.wall_list = arcade.SpriteList()
+        self.coin_list = arcade.SpriteList()  # **NUEVO** Inicializamos la lista de monedas
 
-        #Reiniciamos la puntuación
+        # Reiniciamos la puntuación
         self.score = 0
 
         # --- DIBUJAMOS EL CIRCUITO ---
+        # **NUEVO** He añadido algunas letras 'C' para poner monedas de prueba
         mapa = [
             "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOO",
-            "O                            O",
+            "O C          C               O",
             "O  XXXX  XXXXXXXXXXXXXXXXXXXXO",
-            "O  X     X                   O",
+            "O  X     X C                 O",
             "O  X  XXXX  X                O",
             "O  X        XXXX           X O",
-            "O                            O",
+            "O        C                   O",
             "O  XXXXXXXXXXXXX   XXXXXXXXX O",
-            "O                            O",
+            "O              C             O",
             "O      XXXXXXXXXXXXXXXX      0",
             "O P                          O",
             "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO"
@@ -65,13 +65,12 @@ class MyGame(arcade.Window):
             for columna_index in range(len(fila)):
                 letra = fila[columna_index]
                 
-                # Calculamos las coordenadas x e y (sirve para ambos tipos de caja)
+                # Calculamos las coordenadas x e y
                 centro_x = columna_index * TAMANO_CAJA
                 centro_y = (len(mapa) - fila_index) * TAMANO_CAJA
                 
                 # Si es una 'O', ponemos la caja del borde exterior
                 if letra == "O":
-                    #Usamos los muros para hacer el recinto
                     wall = arcade.Sprite("lab09-walls/muro.png", SPRITE_SCALING_BOX)
                     wall.center_x = centro_x
                     wall.center_y = centro_y
@@ -79,49 +78,50 @@ class MyGame(arcade.Window):
                     
                 # Si es una 'X', ponemos la caja del laberinto
                 elif letra == "X":
-                    #Usamos las cajas para hacer el laberinto
                     wall = arcade.Sprite("lab09-walls/caja.png", SPRITE_SCALING_BOX)
                     wall.center_x = centro_x
                     wall.center_y = centro_y
                     self.wall_list.append(wall)
 
-                #En la letra P se va a situar el jugador
-                elif letra == "P":
-                    #Creamos el jugador
-                    self.player_sprite = arcade.Sprite("lab09-walls/jugador.png", SPRITE_SCALING_PLAYER)
+                # **NUEVO** Si es una 'C', ponemos una moneda
+                elif letra == "C":
+                    # ¡Asegúrate de tener una imagen para la moneda en tu carpeta!
+                    coin = arcade.Sprite("lab09-walls/moneda.png", SPRITE_SCALING_COIN) 
+                    coin.center_x = centro_x
+                    coin.center_y = centro_y
+                    self.coin_list.append(coin)
 
-                    #Le damos las coordenadas exactas de la 'P'
+                # En la letra P se va a situar el jugador
+                elif letra == "P":
+                    self.player_sprite = arcade.Sprite("lab09-walls/jugador.png", SPRITE_SCALING_PLAYER)
                     self.player_sprite.center_x = centro_x
                     self.player_sprite.center_y = centro_y
-                    print(f"Jugador colocado en: {centro_x}, {centro_y}")
-
-                    #Lo añadimos a la lista de cosas a dibujar
                     self.player_list.append(self.player_sprite)
 
-        #Creamos el motor de física: esto identifica al jugador y a un alista de sprites por los que el jugador no tiene permitido pasar
+        # Creamos el motor de física
         self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite, self.wall_list)
        
-        #Creamos dos cámaras: una para el mundo exterior y otra para la puntuación (o cualquier elemento de la interaz)
+        # Creamos dos cámaras
         self.camera_for_sprites = arcade.Camera2D()
         self.camera_for_gui = arcade.Camera2D()
  
     def on_draw(self):
         self.clear()        
         
-        #Selccionamos la cámara para nuestros sprites
+        # Seleccionamos la cámara para nuestros sprites
         self.camera_for_sprites.use()
         
-        #Dibujamos los Sprites
+        # Dibujamos los Sprites
         self.wall_list.draw()
+        self.coin_list.draw()  # **NUEVO** Dibujamos las monedas
         self.player_list.draw()
 
-        #Seleccionamos la cámara para los elementos de la interfaz
+        # Seleccionamos la cámara para los elementos de la interfaz
         self.camera_for_gui.use()
         arcade.draw_text(f"Score: {self.score}", 10, 10, arcade.color.WHITE, 24)
 
     def on_key_press(self, key, modifiers):
         """Función que se ejecuta cada vez que se pulsa una tecla"""
-
         if key == arcade.key.UP:
             self.player_sprite.change_y = MOVEMENT_SPEED
         elif key == arcade.key.DOWN:
@@ -133,7 +133,6 @@ class MyGame(arcade.Window):
 
     def on_key_release(self, key, modifiers):
         """Función que se ejecuta cada vez que se suelta una tecla"""
-
         if key == arcade.key.UP or key == arcade.key.DOWN:
             self.player_sprite.change_y = 0
         elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
@@ -144,14 +143,21 @@ class MyGame(arcade.Window):
         
         self.physics_engine.update()
 
-        #Centramos la cámara en el jugador
+        # **NUEVO** Comprobamos si el jugador ha tocado alguna moneda
+        monedas_chocadas = arcade.check_for_collision_with_list(self.player_sprite, self.coin_list)
+
+        # **NUEVO** Recorremos todas las monedas con las que hemos chocado
+        for moneda in monedas_chocadas:
+            moneda.remove_from_sprite_lists() # La moneda desaparece de la pantalla
+            self.score += 10 # Sumamos 10 puntos por cada moneda (puedes cambiar este valor)
+
+        # Centramos la cámara en el jugador
         self.camera_for_sprites.position = self.player_sprite.position
 
 def main():
     window = MyGame()
     window.setup()
     arcade.run()
-
 
 if __name__ == "__main__":
     main()
